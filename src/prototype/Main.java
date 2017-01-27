725cf1087d057b60a85f6382a7c04a152d0e065c
Victor Nascimento Bakke <gipphe@gmail.com>
2017-01-27 17:20:13 +0100

    Add main loop in Main class

--- src/prototype/Main.java
+++ src/prototype/Main.java
@@ -1,13 +1,18 @@
 package prototype;
 
-/**
- * Created by Jonas on 27.01.2017.
- */
 public class Main {
-
-
     public static void main(String[] args){
         Grid grid = new Grid();
+
+        try {
+            while(true) {
+                grid.tick();
+                System.out.println(grid);
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         System.out.println(grid);
     }
 }
