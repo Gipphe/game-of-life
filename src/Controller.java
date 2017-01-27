3fc1e94f2226cb7bf225a3fa86495c3df4dfb0cc
Victor Nascimento Bakke <gipphe@gmail.com>
2017-01-27 13:36:34 +0100

    Setup for displaying the app

--- src/Controller.java
+++
@@ -1,7 +0,0 @@
-package sample;
-
-public class Controller {
-    public static void main(String[] args) {
-        System.out.println("Foobar");
-    }
-}
--- src/app/Main.java
+++ src/app/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package app;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("window.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
 
