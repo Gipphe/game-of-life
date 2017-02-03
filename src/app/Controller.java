6cdfd304202ee1631f509d3d0e84baefdc8f8bbf
Victor Nascimento Bakke <gipphe@gmail.com>
2017-02-03 15:19:22 +0100

    Canvas now updates from grid every JafaFX tick

--- src/app/Controller.java
+++ src/app/Controller.java
@@ -1,26 +1,48 @@
 package app;
 
+import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.paint.Color;
 
+import java.awt.*;
+
 public class Controller {
     private Grid grid = new Grid();
     private byte scale = 15;
+    private AnimationTimer timer;
 
     @FXML
     private Canvas canvas;
     @FXML
     private ToggleButton startStopButton;
 
-
+    public void toggleStartStop() {
+        if (startStopButton.selectedProperty().getValue()) {
+            start();
+        } else {
+            stop();
+        }
+    };
     public void start() {
-        GraphicsContext gc = canvas.getGraphicsContext2D();
-        drawGame(gc);
-        grid.tick();
-        System.out.println(grid);
+        final GraphicsContext gc = canvas.getGraphicsContext2D();
+        timer = new AnimationTimer() {
+            private long past;
+            @Override
+            public void handle(long now) {
+                if (now - past < 500000000) return;
+                past = now;
+
+                grid.tick();
+                drawGame(gc);
+            }
+        };
+        timer.start();
+    }
+    public void stop() {
+        timer.stop();
     }
     private void drawGrid(GraphicsContext gc) {
         Canvas canvas = gc.getCanvas();
@@ -55,9 +77,6 @@ public class Controller {
                 gc.fillRect(x * scale, y * scale, scale, scale);
             }
         }
-    }
-    public void stop() {
-
     }
 
     public void exit() {
