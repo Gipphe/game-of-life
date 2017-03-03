e216716a1fe214a99fed5fef22bddc6be3f66c9c
yanislavzagorov <yanislavzagorov@yahoo.com>
2017-03-03 16:24:38 +0100

    Wrote test for nextGeneration

--- src/test/GridTest.java
+++ src/test/GridTest.java
@@ -1,31 +1,22 @@
 package Test;
 
+import app.Grid;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-/**
- * Created by yanis on 03-Mar-17.
- */
 class GridTest {
     @Test
     void nextGeneration() {
+        byte[][] testBoard = {
+                { 0, 0, 0, 0 },
+                { 0, 1, 1, 0 },
+                { 0, 1, 1, 0 },
+                { 0, 0, 0, 0 }
+        };
+        Grid testGrid = new Grid(4,4);
+        testGrid.setBoard(testBoard);
+        testGrid.nextGeneration();
 
-    }
-
-    @Test
-    void toString() {
-
-    }
-
-    @Test
-    void setBoard() {
-
-    }
-
-    @Test
-    void getGrid() {
-
+        org.junit.jupiter.api.Assertions.assertEquals(testGrid.toString(),"0000011001100000");
     }
 
 }
