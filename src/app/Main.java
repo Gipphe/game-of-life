26ceb57acaf52f3e01ea3f64c013cd907ad49006
yanislavzagorov <yanislavzagorov@yahoo.com>
2017-03-06 13:32:46 +0100

    Program scene size now relevant to screen size (2/3 of total width/height)

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,14 +4,16 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
+        javafx.geometry.Rectangle2D visualBounds = Screen.getPrimary().getVisualBounds();
 
         Parent root = FXMLLoader.load(getClass().getResource("window.fxml"));
-        Scene primaryScene = new Scene(root, 600, 400);
+        Scene primaryScene = new Scene(root, visualBounds.getWidth()/1.5, visualBounds.getHeight()/1.5);
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(primaryScene);
         primaryStage.show();
