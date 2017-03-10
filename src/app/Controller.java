7b7ea1755a378f1626cdd2d8f76f06f9d0cadd20
yanislavzagorov <yanislavzagorov@yahoo.com>
2017-03-10 13:55:27 +0100

    Revert "Switches X/Y axis"
    This reverts commit 542bef7dc5fd0e603e26814369653d89c66f1391.

--- src/app/Controller.java
+++ src/app/Controller.java
@@ -49,7 +49,7 @@ public class Controller implements Initializable {
     }
 
     public void resetGame(){
-        this.grid= new Grid(grid.yaxis, grid.xaxis);
+        this.grid= new Grid(grid.xaxis, grid.yaxis);
         aliveColor = Color.BLACK;
         deadColor = Color.WHITE;
         draw(gc);
@@ -139,10 +139,10 @@ public class Controller implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        int yaxis = 20;
         int xaxis = 20;
-        grid = new Grid(yaxis, xaxis);
-        byte[][] foo = new byte[yaxis][xaxis];
+        int yaxis = 20;
+        grid = new Grid(xaxis, yaxis);
+        byte[][] foo = new byte[xaxis][yaxis];
 
         try {
             Pattern shape = PatternCollection.getCollection()[5];
@@ -176,7 +176,7 @@ public class Controller implements Initializable {
         scaleSlider.valueProperty().addListener(new ChangeListener<Number>() {
             @Override
             public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-                gc.clearRect(0,0,scale*grid.yaxis,scale*grid.xaxis);
+                gc.clearRect(0,0,scale*grid.xaxis,scale*grid.yaxis);
                 setScale(newValue.byteValue());
                 draw(gc);
             }
