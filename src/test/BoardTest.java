0296c2d56bb2617fe919970ac3bcdf6851a8176e
Jonas Iversen <s300766@stud.hioa.no>
2017-03-10 14:10:57 +0100

    Renamed grid to board.

--- src/test/BoardTest.java
+++ src/test/BoardTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class GridTest {
+class BoardTest {
     @Test
     void squares_remain_squares() {
         byte[][] testBoard = {
