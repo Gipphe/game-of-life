d6c5fe15adfe897d68c6b88305e96c62a2cf7849
Victor Nascimento Bakke <gipphe@gmail.com>
2017-03-23 13:19:52 +0100

    Add javadoc to PatternCollection. Redid patterns

--- src/app/PatternCollection.java
+++ src/app/PatternCollection.java
@@ -1,52 +1,144 @@
 package app;
 
+/**
+ * Container class for Patterns
+ *
+ * Holds the name and actual array information for the specified pattern.
+ */
 class Pattern {
+    /**
+     * Name of the pattern.
+     */
     private final String name;
+
+    /**
+     * Actual pattern table.
+     */
     private final byte[][] pattern;
 
-    public Pattern(String name, byte[][] pattern) {
+    /**
+     * Horizontal size of the pattern.
+     */
+    private final int sizeX;
+
+    /**
+     * Vertical size of the pattern.
+     */
+    private final int sizeY;
+
+    /**
+     * Constructor
+     * @param name Name of the pattern in question.
+     * @param pattern Two-dimensional array representing the pattern.
+     */
+    Pattern(String name, byte[][] pattern) {
         this.name = name;
         this.pattern = pattern;
+        this.sizeY = pattern.length;
+        if (pattern.length == 0) {
+            this.sizeX = 0;
+        } else {
+            this.sizeX = pattern[0].length;
+        }
     }
 
+    /**
+     * Getter for the name.
+     * @return Name of the pattern.
+     */
     public String getName() {
         return name;
     }
+
+    /**
+     * Getter for the pattern information.
+     * @return The pattern array.
+     */
     public byte[][] getPattern() {
         return pattern;
     }
-}
 
-public class PatternCollection {
-    private static final Pattern Clear;
-    private static final Pattern Glider;
-    private static final Pattern Blinker;
-    private static final Pattern Toad;
-    private static final Pattern Beacon;
-    private static final Pattern Pulsar;
-    private static final Pattern Pentadecathlon;
-    private static final Pattern LightweightSpaceship;
-    private static final Pattern[] Collection;
-
-    static {
-        Clear = new Pattern("Clear", new byte[][] {});
-        Glider = new Pattern("Glider", new byte[][] {{0,2}, {1,0}, {1,2}, {2,1}, {2,2}});
-        Blinker = new Pattern("Blinker", new byte[][] {{0,1}, {1,1}, {2,1}});
-        Toad = new Pattern ("Toad", new byte[][] {{0,1}, {1,0}, {1,1}, {2,0}, {2,1}, {3,0}});
-        Beacon = new Pattern ("Beacon", new byte[][] {{0,0}, {0,1}, {1,0}, {2,3}, {3,2}, {3,3}});
-        Pulsar = new Pattern ("Pulsar", new byte[][] {{0,2}, {0,3}, {0,4}, {0,8}, {0,9}, {0,10},
-                {2,0}, {2,5}, {2,7}, {2,12}, {3,0}, {3,5}, {3,7}, {3,12}, {4,0},
-                    {4,5}, {4,7}, {4,12}, {5,2}, {5,3}, {5,4}, {5,8}, {5,9}, {5,10},
-                    {7,2}, {7,3}, {7,4}, {7,8}, {7,9}, {7,10}, {8,0}, {8,5}, {8,7},
-                    {8,12}, {9,0}, {9,5}, {9,7}, {9,12}, {10,0},
-                    {10,5}, {10,7}, {10,12}, {12,2}, {12,3}, {12,4}, {12,8}, {12,9}, {12,10}});
-        Pentadecathlon = new Pattern ("Pentadecathlon", new byte[][] {{0,1}, {1,1}, {2,0}, {2,2}, {3,1},
-                    {4,1}, {5,1}, {6,1}, {7,0}, {7,2}, {8,1}, {9,1}});
-        LightweightSpaceship = new Pattern ("LightweightSpaceship", new byte[][] {{0,1}, {0,3}, {1,0}, {2,0}, {3,0},
-                    {3,3}, {4,0}, {4,1}, {4,2}});
-        Collection = new Pattern[] {Clear, Glider, Blinker, Toad, Beacon, Pulsar, Pentadecathlon, LightweightSpaceship};
+    /**
+     * Getter for the horizontal size.
+     * @return The horizontal size.
+     */
+    public int getSizeX() {
+        return sizeX;
     }
 
+    /**
+     * Getter for the vertical size.
+     * @return The vertical size.
+     */
+    public int getSizeY() {
+        return sizeY;
+    }
+}
+
+/**
+ * Container class for all Patterns.
+ */
+public class PatternCollection {
+    private static final Pattern Clear = new Pattern("Clear", new byte[][] {});
+    private static final Pattern Glider = new Pattern("Glider", new byte[][] {
+            {0,1,0},
+            {1,0,0},
+            {1,1,1}
+    });
+    private static final Pattern Blinker = new Pattern("Blinker", new byte[][] {
+            {1,1,1}
+    });
+    private static final Pattern Toad = new Pattern("Toad", new byte[][]{
+            {0,1,1,1},
+            {1,1,1,0}
+    });
+    private static final Pattern Beacon = new Pattern("Beacon", new byte[][] {
+            {1,1,0,0},
+            {1,0,0,0},
+            {0,0,0,1},
+            {0,0,1,1}
+    });
+    private static final Pattern Pulsar = new Pattern("Pulsar", new byte[][] {
+            {0,0,1,1,1,0,0,0,1,1,1,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {1,0,0,0,0,1,0,1,0,0,0,0,1},
+            {1,0,0,0,0,1,0,1,0,0,0,0,1},
+            {1,0,0,0,0,1,0,1,0,0,0,0,1},
+            {0,0,1,1,1,0,0,0,1,1,1,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,1,1,1,0,0,0,1,1,1,0,0},
+            {1,0,0,0,0,1,0,1,0,0,0,0,1},
+            {1,0,0,0,0,1,0,1,0,0,0,0,1},
+            {1,0,0,0,0,1,0,1,0,0,0,0,1},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,1,1,1,0,0,0,1,1,1,0,0}
+    });
+    private static final Pattern Pentadecathlon = new Pattern("Pentadecathlon", new byte[][] {
+            {0,0,1,0,0,0,0,1,0,0},
+            {1,1,0,1,1,1,1,0,1,1},
+            {0,0,1,0,0,0,0,1,0,0}
+    });
+    private static final Pattern LightweightSpaceship = new Pattern("Lightweight spaceship", new byte[][] {
+            {0,1,0,0,1},
+            {1,0,0,0,0},
+            {1,0,0,0,1},
+            {1,1,1,1,0}
+    });
+    private static final Pattern[] Collection = new Pattern[] {
+            Clear,
+            Glider,
+            Blinker,
+            Toad,
+            Beacon,
+            Pulsar,
+            Pentadecathlon,
+            LightweightSpaceship
+    };
+
+    /**
+     * Getter for the entire pattern collection.
+     * @return An array with the collection.
+     */
     public static Pattern[] getCollection() {
         return Collection;
     }
