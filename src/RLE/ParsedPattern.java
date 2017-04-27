abbf27c747c674544ccc51b0bfa9d7e81f46c2e8
Victor Nascimento Bakke <gipphe@gmail.com>
2017-04-27 19:53:56 +0200

    Optimized pattern to RLE parser
    Now handles the passed pattern directly instead of juggling types through
    stacks of strings and the like. Also accumulates through integer counters
    instead of StringBuilders that are subsequently accumulated. Makes MUCH
    more sense to use counters, which is exactly what run counts are...

--- src/RLE/ParsedPattern.java
+++ src/RLE/ParsedPattern.java
@@ -1,16 +1,41 @@
 package RLE;
 
+/**
+ * Container for an RLE to Board parsing's output, containing the parsed pattern and the rule the pattern is designed
+ * for. Can be expanded to include metadata such as author, creation date etc.
+ */
 public class ParsedPattern {
+    /**
+     * The rule designation that this pattern is designed for.
+     */
     private String rule;
+    /**
+     * The pattern itself, represented as a two-state two-dimensional byte array for simplicity.
+     */
     private byte[][] pattern;
 
+    /**
+     * Constructor
+     * @param rule The rule this pattern is designed for.
+     * @param pattern The pattern itself.
+     */
     ParsedPattern(String rule, byte[][] pattern) {
         this.rule = rule;
         this.pattern = pattern;
     }
+
+    /**
+     *
+     * @return The rule this pattern is designed for.
+     */
     public String getRule() {
         return rule;
     }
+
+    /**
+     *
+     * @return The pattern itself.
+     */
     public byte[][] getPattern() {
         return pattern;
     }
--- src/RLE/Parser.java
+++ src/RLE/Parser.java
@@ -1,9 +1,5 @@
 package RLE;
 
-import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Parser {
     /**
      * Converts a passed Parser string to a byte[][] model for consumption by the Board class.
@@ -13,8 +9,8 @@ public class Parser {
     public static ParsedPattern toPattern(String RLEString) {
         FileContents contents = new FileContents(RLEString);
 
-        int sizeX = contents.getX();
-        int sizeY = contents.getY();
+        int sizeX = contents.getNumCols();
+        int sizeY = contents.getNumRows();
         String rule = contents.getRule();
 
         byte[][] pattern = new byte[sizeY][sizeX];
@@ -69,107 +65,111 @@ public class Parser {
     }
 
     /**
-     * Counts number of characters in string, and reduces them to a number, representing the number of occurrences of
-     * that string in the source string, and a single instance of the character in question.
-     *
-     * @param source Source string to reduce to number + character.
-     * @return Reduced string with the number of occurrences of the character in the source string.
-     */
-    private static String concentrate(String source) {
-        int length = source.length();
-        String lengthString = length == 1 ? "" : "" + length;
-        return lengthString + source.charAt(0);
-    }
-
-    /**
      * Finds out whether the passed row is made up entirely of dead cells or not.
      * @param row Row to check.
      * @return True if the row is empty, false otherwise.
      */
-    private static boolean isEmptyRow(String row) {
-        Pattern pattern = Pattern.compile("[^0,\\]\\[ ]");
-        Matcher match = pattern.matcher(row);
-        boolean isEmpty = true;
-        if (match.find()) {
-            isEmpty = false;
+    private static boolean isEmptyRow(byte[] row) {
+        for (byte col : row) {
+            if (col == 1) return false;
         }
-        return isEmpty;
+        return true;
     }
 
     /**
-     * Parses passed model into Parser for consumption by a file writer.
-     * @param board Board to convert to an Parser string.
+     * Finds the index of the last alive cell in the passed row.
+     * @param row Row to find the last alive cell in.
+     * @return Index of the last alive cell.
+     */
+    private static int lastAliveInRow(byte[] row) {
+        int index = 0;
+        for (int i = 0; i < row.length; i++) {
+            if (row[i] == 1) index = i;
+        }
+        return index;
+    }
+
+    /**
+     * Parses passed model into RLE for consumption by a file writer.
+     * @param pattern Pattern to convert to an RLE string.
      * @return Parser string representing the model.
      */
-    public static String fromPattern(byte[][] board) {
-        PatternContents rBoard = new PatternContents(board);
-
+    public static String fromPattern(byte[][] pattern) {
         StringBuilder result = new StringBuilder();
         StringBuilder line = new StringBuilder();
         line
                 .append("x = ")
-                .append(rBoard.getX())
+                .append(pattern[0].length)
                 .append(", y = ")
-                .append(rBoard.getY())
+                .append(pattern.length)
                 .append(", rule = B3/S23");
         result
                 .append(line.toString())
                 .append("\n");
         line.setLength(0);
 
-        // Accumulator for counting newline characters.
-        StringBuilder newlineCharacters = new StringBuilder();
+        // Counter for newline characters.
+        int newLineCount = 1;
 
-        Stack<String> rows = rBoard.getRows();
-        int lastRowIndex = rows.size() - 1;
-        for (int rowIndex = 0; rowIndex < rows.size(); rowIndex++) {
-            String row = rows.get(rowIndex);
-            // If the row is empty, increment newline character count
+        int lastRowIndex = pattern.length - 1;
+        for (int rowIndex = 0; rowIndex < pattern.length; rowIndex++) {
+            byte[] row = pattern[rowIndex];
+            // If the row is empty, increment newline character count.
             if (isEmptyRow(row)) {
-                newlineCharacters.append("$");
+                newLineCount++;
                 continue;
             }
-            // If this is not the first row, append newline characters
+            // If this is not the first row, append newline character count (if applicable) and newline character ($).
             if (rowIndex != 0) {
-                newlineCharacters.append("$");
-                line.append(concentrate(newlineCharacters.toString()));
-                newlineCharacters.setLength(0);
+                if (newLineCount > 1) {
+                    line.append(newLineCount);
+                    newLineCount = 1;
+                }
+                line.append("$");
             }
-            String[] statesInRow = row.split("");
 
-            StringBuilder stateCharacters = new StringBuilder();
-            int indexOfLastState = statesInRow.length - 1;
+            // Counter for a specific state.
+            int stateCount = 1;
+            int indexOfLastState = row.length - 1;
 
-            int colsToRun = statesInRow.length;
-            // If this is the last row, only include up to and including the last alive cell, omitting the last dead cells
+            // Number of iterations for this row.
+            int colsToIterate = row.length;
+            // If it is the last row, only include up to and including the last alive cell, omitting the remaining
+            // dead cells.
             if (rowIndex == lastRowIndex) {
-                int lastAlive = row.lastIndexOf('1');
-                colsToRun = (lastAlive == -1) ? statesInRow.length : lastAlive + 1;
+                int lastAliveIndex = lastAliveInRow(row);
+                colsToIterate = (lastAliveIndex == -1) ? row.length : lastAliveIndex + 1;
             }
 
-            // Iterate through cells in the row
-            for (int i = 0; i < colsToRun; i++) {
-                String state = statesInRow[i];
-                String value = (state.equals("1") ? "o" : "b");
+            // Iterate through cells in the row.
+            for (int i = 0; i < colsToIterate; i++) {
+                byte state = row[i];
+                String tag = (state == 1 ? "o" : "b");
 
-                String nextState = "";
+                byte nextState = -1;
                 if (i < indexOfLastState) {
-                    nextState = statesInRow[i + 1];
+                    nextState = row[i + 1];
                 }
 
-                if (state.equals(nextState)) {
-                    stateCharacters.append(value);
+                // If this state is the same as the next state, iterate state counter.
+                if (state == nextState) {
+                    stateCount++;
                     continue;
                 }
-                stateCharacters.append(value);
-                String element = concentrate(stateCharacters.toString());
-                if (line.length() + element.length() > 70) {
+                StringBuilder runCountWithTagBuilder = new StringBuilder();
+                if (stateCount > 1) {
+                    runCountWithTagBuilder.append(stateCount);
+                    stateCount = 1;
+                }
+                runCountWithTagBuilder.append(tag);
+
+                String runCountWithTag = runCountWithTagBuilder.toString();
+                if (line.length() + runCountWithTag.length() > 70) {
                     result.append(line.toString())
                             .append("\n");
                     line.setLength(0);
                 }
-                line.append(element);
-                stateCharacters.setLength(0);
+                line.append(runCountWithTag);
             }
         }
         result.append(line.toString());
