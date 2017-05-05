bcc1dfbbe429d3de188f4458967f7b9c6c1d9b35
Victor Nascimento Bakke <gipphe@gmail.com>
2017-05-05 17:37:06 +0200

    Add tests and JavaDoc to utils

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -1,10 +1,32 @@
 package utils;
 
-public class Utils {
+public final class Utils {
+    /**
+     * Hog the constructor to prevent instantiation.
+     */
+    private Utils() {}
+
+    /**
+     * Limits the value to the passed max and min values.
+     * @see Utils#limit(double, double, double) 
+     * @param max Maximum value, inclusive, for {@code value}.
+     * @param min Minimum value, inclusive, for {@code value}.
+     * @param value Value to limit.
+     * @return The value if it is within the boundaries, or the boundary it exceeds itself.
+     */
     public static int limit(int max, int min, int value) {
         return (int) Math.round(limit((double) max, (double) min, (double) value));
     }
+
+    /**
+     * Limits the value to the passed max and min values.
+     * @param max Maximum value, inclusive, for {@code value}.
+     * @param min Minimum value, inclusive, for {@code value}.
+     * @param value Value to limit.
+     * @return The value if it is within the boundaries, or the boundary it exceeds itself.
+     */
     public static double limit(double max, double min, double value) {
+        if (max < min) throw new RuntimeException("Max is less than min");
         return Math.max(Math.min(value, max), min);
     }
 
@@ -17,6 +39,8 @@ public class Utils {
      * @return Value within [0, lim].
      */
     public static int wrap(int max, int min, int val) {
+        if (max < min) throw new RuntimeException("Max is less than min");
+
         if (val >= max) {
             return val - max;
         } else if (val < min) {
